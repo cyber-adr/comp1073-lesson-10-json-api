a847d1277ddf2c20f1090d74c21ded1557bad496
Neil Adrian Mendoza <naimnz.7780@gmail.com>
2025-11-17 13:34:37 -0500

    Lesson 10 API Server Hosting

--- js/json.js
+++ js/json.js
@@ -1,23 +1,24 @@
 /* STEP 2: Reference the HEADER and the SECTION elements with variables */
-
+const header = document.querySelector('header')
+const section = document.querySelector('section')
 
 // STEP 3a: Create the asynchronous function populate()
-
+async function Populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    
+    const requestURL = 'https://cyber-adr.github.io/comp1073-lesson-10-json-api/js/i-scream.json'
     // STEP 5: Use the new URL to create a new request object
-    
+    const request = new Request(requestURL)
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
+    const response = await fetch(request)
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
+    const iScream = response.json()
     // STEP 8: Output the iScream JSON object to the console 
-    
+    console.log(iScream)
     // STEP 9a: Invoke the populateHeader function here, then build it below
     
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
+}
 
 
 // STEP 3b: Call the populate() function
